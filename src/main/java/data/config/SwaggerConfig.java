8b42bcc7205c63e97e0a15252bf7001f75b3b873
kkookkss <pre0919@gmail.com>
2024-07-18 17:21:05 +0900

    ✨ Swagger 설정에 JWT Bearer 토큰 인증 추가 - OpenAPI 컴포넌트에 SecurityScheme 추가하여 Bearer 토큰 설정 - 모든 엔드포인트에 bearerAuth 적용하도록 SecurityRequirement 구성 - SwaggerConfig에 인증 설정 추가

--- src/main/java/data/config/SwaggerConfig.java
+++ src/main/java/data/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +16,12 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .info(apiInfo());
     }
 

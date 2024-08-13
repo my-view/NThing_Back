17122cd9c0f34950ab019353d02f6fcd1624a69d
kkookkss <pre0919@gmail.com>
2024-08-13 15:20:33 +0900

    :hammer: LoginFilter 수정 - @ControllerAdvice로 처리할 수 없는 필터 레벨의 예외를 직접 처리하도록 개선 - 액세스 토큰이 null인 경우 UnauthorizedException 발생 - 유효기간이 만료된 토큰에 대해 AccessTokenExpiredException 발생

--- src/main/java/data/filter/LoginFilter.java
+++ src/main/java/data/filter/LoginFilter.java
@@ -1,10 +1,13 @@
 package data.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import data.constants.ErrorCode;
+import data.dto.ErrorResponse;
 import data.exception.AccessTokenExpiredException;
 import data.exception.UnauthorizedException;
 import data.util.JwtProvider;
 import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.MediaType;
 import org.springframework.util.PatternMatchUtils;
 
 import javax.servlet.*;
@@ -12,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class LoginFilter implements Filter{
+public class LoginFilter implements Filter {
 
     private JwtProvider jwtProvider;
     public LoginFilter(JwtProvider jwtProvider) {
@@ -45,7 +48,11 @@ public class LoginFilter implements Filter{
     }; // 추가 필요
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
         HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -58,20 +65,36 @@ public class LoginFilter implements Filter{
 
         String accessToken = httpRequest.getHeader("Authorization");
 
-        if(isLoginCheckPath(requestURI)) { // 검증해야하는 URI인 경우
-            if (accessToken != null) { // 엑세스 토큰이 null이 아니면
-//                chain.doFilter(request, response);
-                jwtProvider.parseJwt(accessToken);
-//                try {
-//                    jwtProvider.parseJwt(accessToken);
-//                } catch (ExpiredJwtException e) {
-//                    throw new AccessTokenExpiredException("access token expired", ErrorCode.ACCESS_TOKEN_EXPIRED);
-//                }
-            } else { // 엑세스 토큰이 없는 경우
-                throw new UnauthorizedException("null token", ErrorCode.UNAUTHORIZED);
+        try {
+            if (isLoginCheckPath(requestURI)) { // 검증해야하는 URI인 경우
+                if (accessToken == null) { // 엑세스 토큰이 null이 아니면
+                    throw new UnauthorizedException("null token", ErrorCode.UNAUTHORIZED);
+                }
+                if (!jwtProvider.isValidAccessToken(accessToken)) { // 유효하지 않은 토큰의 경우
+                    throw new AccessTokenExpiredException("invalid token", ErrorCode.ACCESS_TOKEN_EXPIRED);
+                }
             }
+            chain.doFilter(request, response);
+        } catch (UnauthorizedException e) {
+            setErrorResponse(httpResponse, ErrorCode.UNAUTHORIZED);
+        } catch (ExpiredJwtException | AccessTokenExpiredException e) {
+            setErrorResponse(httpResponse, ErrorCode.ACCESS_TOKEN_EXPIRED);
         }
-        chain.doFilter(request, response);
+    }
+
+    private void setErrorResponse(
+            HttpServletResponse response,
+            ErrorCode errorCode
+    ) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(errorCode.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ErrorResponse errorResponse = new ErrorResponse(errorCode);
+        try {
+            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /* 화이트리스트는 인증 체크를 하지 않음 */
